fe6ae19673ba8e4a13c23e2fb49e850273d9df9d
MesiAnandaPutri <mesianandaputri24@gmail.com>
2025-10-17 17:08:39 +0700

    memanggil fungsi tata letak

--- app/src/main/java/com/example/questbasiccomposable_035/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_035/MainActivity.kt
@@ -1,1 +1,47 @@
 package com.example.questbasiccomposable_035
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.questbasiccomposable_035.ui.theme.QuestBasicComposable_035Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QuestBasicComposable_035Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    TataLetakBoxColumnRow(
+
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    QuestBasicComposable_035Theme {
+        Greeting("Android")
+    }
+}
