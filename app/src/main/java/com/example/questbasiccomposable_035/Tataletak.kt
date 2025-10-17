e515ff904f354be2adf0e37808e2fc09f2dab461
MesiAnandaPutri <mesianandaputri24@gmail.com>
2025-10-17 17:09:13 +0700

    update

--- app/src/main/java/com/example/questbasiccomposable_035/Tataletak.kt
+++ app/src/main/java/com/example/questbasiccomposable_035/Tataletak.kt
@@ -106,7 +106,7 @@ fun TataLetakRowColumn(modifier: Modifier) {
 
 @Composable
 fun TataLetakBoxColumnRow(modifier: Modifier) {
-    val gambar = painterResource(id = R.drawable.musik)
+    val gambar = painterResource(id = R.drawable.meow)
 
     Column {
         Box(
